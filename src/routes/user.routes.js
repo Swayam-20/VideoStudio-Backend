b78960b6f90d3fe3581157be149db11723e8d53f
SWAYAMJAISWAL <jaiswalswayam.jaiswal@gmail.com>
2025-07-17 20:59:14 +0530

    debug

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,6 +1,6 @@
 import {Router} from 'express';
 import { registeruser } from '../controllers/user.controllers.js';
-import upload from '../utils/multer.js'; // Assuming you have a multer setup for file uploads
+import upload from '../middlewares/multer.middleware.js'; // Assuming you have a multer setup for file uploads
 const router = Router();
 
 router.route("/register").post(
@@ -8,6 +8,6 @@ router.route("/register").post(
         { name: 'avatar', maxCount: 1 }, // Avatar upload
         { name: 'coverImage', maxCount: 1 } // Cover image upload
     ]), // Specify the fields you want to upload, e.g., [{ name: 'avatar' }, { name: 'coverImage' }]
-    
+
     registeruser);
 export default router;
