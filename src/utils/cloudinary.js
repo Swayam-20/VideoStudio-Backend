d884c9289097eb3ae27c96571d76113e5fef9ccb
SWAYAMJAISWAL <jaiswalswayam.jaiswal@gmail.com>
2025-07-17 13:01:59 +0530

    setup cloudinary and some middleware

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,32 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from 'fs'; 
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, // Click 'View API Keys' above to copy your cloud name
+        api_key: process.env.CLOUDINARY_API_KEY, // Click 'View API Keys' above to copy your API key
+        api_secret: process.env.CLOUDINARY_API_SECRET// Click 'View API Keys' above to copy your API secret
+    });
+    
+    // Function to upload a file to Cloudinary
+    const uploadfile = async (localfilepath) => {
+        try{if (!localfilepath) {
+            return null;
+        }
+            const res = await cloudinary.uploader.upload(localfilepath, {
+                resource_type:"auto"
+        })
+        // file uploaded successfully
+        console.log("File uploaded successfully:", res);
+        
+        return res;
+
+    }
+    catch(error){
+        fs.unlinkSync(localfilepath); // Delete the local file if upload fails from local storage
+        return null;
+
+    }
+}
+    
