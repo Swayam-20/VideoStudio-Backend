3ae8c99685ce220a0061c1a85e89d51975ae770f
SWAYAMJAISWAL <jaiswalswayam.jaiswal@gmail.com>
2025-07-17 22:39:37 +0530

    Update multer.middleware.js

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,8 +2,8 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './public/temp')// Specify the destination folder for temporary storage
-    },
+      cb(null, './public/temp'//path  // Specify the destination folder for temporary storage
+    )},
     filename: function (req, file, cb) {
       
       cb(null, file.originalname)
