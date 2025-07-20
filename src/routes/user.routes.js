8c1f267f41441f1c3b80c08ab8ee6a0bcff4b427
SWAYAMJAISWAL <jaiswalswayam.jaiswal@gmail.com>
2025-07-21 00:30:55 +0530

    get routes set for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,15 @@
 import {Router} from 'express';
-import { loggoutuser, loginuser, RefreshAccessToken, registeruser } from '../controllers/user.controllers.js';
+import { RefreshAccessToken,
+    registeruser,
+    loginuser ,
+    loggoutuser ,
+    changecurrentpassword ,
+    getcurrentuser,
+    updateUserDetail,
+    updateUserAvatar,
+    updateUserCoverImage,
+    getUserChannelprofile,
+    getwatchHistory } from '../controllers/user.controllers.js';
 import {upload} from '../middlewares/multer.middleware.js'; // Assuming you have a multer setup for file uploads
 
 import verifyJWT from '../middlewares/auth.middleware.js';
@@ -15,5 +25,12 @@ router.route("/register").post(
 
 router.route("/login").post(loginuser);
 router.route("/logout").post(verifyJWT,loggoutuser);
-router.route("/refreshAccessToken").get(RefreshAccessToken)
+router.route("/refreshAccessToken").post(RefreshAccessToken)
+router.route("/change-password").post(verifyJWT,changecurrentpassword)
+router.route("/current-user").get(verifyJWT,getcurrentuser)
+router.route("/update-user-detail").patch(verifyJWT,updateUserDetail)
+router.route("/update-user-avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+router.route("/update-user-cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverImage)
+router.route("/c/:username").get(verifyJWT,getUserChannelprofile)
+router.route("/watch-histroy").get(verifyJWT,getwatchHistory)
 export default router;
